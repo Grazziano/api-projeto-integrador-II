ed0c7cd27b0cecd097e88abd1de494a6c608f2ef
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2026-09-11 10:47:52 -0300

    feat: instalando nodemon e criando rota post

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 
 const server = express();
+server.use(express.json());
+const PORT = 3000;
 
 server.get('/produtos', (req, res) => {
   return res.send('Hello World!');
 });
 
-server.listen(3000, () => {
-  console.log('Servidor Rodando na porta 3000');
+server.get('/produtos/:id', (req, res) => {
+  const id = req.params.id;
+  return res.send(`O tem id = ${id}`);
+});
+
+server.post('/produtos', (req, res) => {
+  return res.send('Produto cadastrado com sucesso!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Servidor Rodando na porta ${PORT}`);
 });
